cd7472af77b819725278f0d93e69f4b40c408f16
Sergey Khistovoy <sergeykhistovoy@gmail.com>
2018-07-02 15:32:20 +0300

    fixed the connection of the language switch

--- full-site/includes/header_ru.php
+++ full-site/includes/header_ru.php
@@ -241,7 +241,7 @@ path.svg-inner_a_1_2 {animation-delay: 4.1s;}
                <a href="<?if(!empty($url_r)){ echo $url_r;}else{ echo $uRr;}?>">RU</a>
                <a href="<?if(!empty($url_n)){ echo $url_n;}else{ echo $uRn;}?>">EN</a>
               </div> */?>
-              <?php /*Google Tag Manager*/ include_once('../includes/lang_m.php'); ?>
+              <?php /*Google Tag Manager*/ include_once('lang_m.php'); ?>
             </li>
       <li class="menu-close">
               <svg style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" width="512px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z"/></svg>
