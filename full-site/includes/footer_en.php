4019d398795300ab0fe4fdaa0598391ba4818b26
Begas Anna <anneta.begas@gmail.com>
2018-08-16 11:32:37 +0300

    instagram

--- full-site/includes/footer_en.php
+++ full-site/includes/footer_en.php
@@ -1,32 +1,47 @@
 	<footer>
        <div class="wrapper">
-				 <span class="copy">© 2017 CHICAGO Central House</span>
-				 <span>marketing department - marketing@saga-development.com.ua</span>
-				 <span><a href="/en/sitemap/"><img <?LazyLoad("/img/home-footer.svg");?> alt="sitemap" width="30px" style="margin-bottom:-3px;"></a></span>
-				 <span class="smarto">
-						 website development
-					  <a  <?LazyLoad("/img/logo-smart.png", $option=array('class'=>'logo-smart'));?>></a>
-				 </span>
-						<span class="fb-foot"><a href="https://www.facebook.com/Chicago-Central-House-1836472106584585" target="_blank"><img src="/img/icons/Facebook-04.svg" alt="fb" / width="50px" height="50px"></a></span>
+				 <span class="copy">(c) 2017 CHICAGO Central House</span>
+				 <span class="dep_market">marketing department - marketing@saga-development.com.ua</span>
+				 <div class="soc_links flex">
+					 <a href="/en/sitemap/" style="margin-right:16px;">
+						 <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 512 512" fill="#999999"height="26" viewBox="0 0 512 512" width="26"><path d="m512 296-96-96v-144h-64v80l-96-96-256 256v16h64v160h160v-96h64v96h160v-160h64z"/></svg>
+					 </a>
+					 <a href="https://www.facebook.com/Chicago-Central-House-1836472106584585" target="_blank">
+						 <svg xmlns="http://www.w3.org/2000/svg" height="26"  width="26" viewBox="0 0 500 500"><path fill="#999999" d="M301.5 84.8h70V0h-98.9c-79.7 16.1-80.7 89-80.7 89v86.4h-63.5v84.1h63.5V500h88.5V259.3h83.4l7.9-84.1H281v-61.7c-.1-27.1 20.5-28.7 20.5-28.7z"/></svg>
+					 </a>
+					 <a href="https://www.instagram.com/chicago.kyiv/" title="instagram" target="_blank" class="instagram">
+						 <svg xmlns="http://www.w3.org/2000/svg" height="26"  width="26" enable-background="new 0 0 512 512" fill="#999999" viewBox="0 0 512 512"><path d="m352 0h-192c-88.352 0-160 71.648-160 160v192c0 88.352 71.648 160 160 160h192c88.352 0 160-71.648 160-160v-192c0-88.352-71.648-160-160-160zm112 352c0 61.76-50.24 112-112 112h-192c-61.76 0-112-50.24-112-112v-192c0-61.76 50.24-112 112-112h192c61.76 0 112 50.24 112 112z"/><path d="m256 128c-70.688 0-128 57.312-128 128s57.312 128 128 128 128-57.312 128-128-57.312-128-128-128zm0 208c-44.096 0-80-35.904-80-80 0-44.128 35.904-80 80-80s80 35.872 80 80c0 44.096-35.904 80-80 80z"/><circle cx="393.6" cy="118.4" r="17.056"/></svg>
+					 </a>
+				 </div>
+				 <div itemscope itemtype="http://schema.org/Organization" style="overflow: hidden; height:0px; width:0;">
+					 <span itemprop="name">CHICAGO CENTRAL HOUSE</span>
+					 <a itemprop="url" href="https://chicago.kiev.ua"></a>
+					 <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+						 <span itemprop="streetAddress">44, Antonovicha street </span>,
+						 <span itemprop="addressLocality">Keiv</span>,
+						 <span itemprop="addressRegion">Kiev region</span>.
+					 </div>
+					 <img itemprop="logo" <?LazyLoad("/img/chicago-logo.png");?> />
+					 <span itemprop="telephone">(044)22-77-244</span>
+					 <span itemprop="email">marketing@saga-development.com.ua</span>
+				 </div>
+
+				 <div class="smarto flex">website development</span><a  <?LazyLoad("/img/logo-smart.png", $option=array('class'=>'logo-smart'));?>></a></div>
 
 
-						<div itemscope itemtype="http://schema.org/Organization" style="overflow: hidden; height:0px;">
-							<span itemprop="name">CHICAGO CENTRAL HOUSE</span>
-							<a itemprop="url" href="https://chicago.kiev.ua"></a>
-							<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-								<span itemprop="streetAddress">44, Antonovicha street </span>,
-								<span itemprop="addressLocality">Keiv</span>,
-								<span itemprop="addressRegion">Kiev region</span>.
-							</div>
-							<img itemprop="logo" <?LazyLoad("/img/chicago-logo.png");?> />
-							<span itemprop="telephone">(044)22-77-244</span>
-							<span itemprop="email">marketing@saga-development.com.ua</span>
-						</div>
 				</div>
   </footer>
 	<style media="screen">
+	footer{	height: auto;	line-height: normal;	padding: 16px 0;}
+	footer .wrapper,footer .flex{
+		display: flex;	justify-content: space-between;	align-items: center;}
+	footer .logo-smart{	top: 0;	margin-left: 5px;}
+	.soc_links a{display: block;width: 36px;height: 36px;padding: 6px;margin: 0 4px;}
+	.soc_links a svg{	width: 100%;	height: auto;}
 	@media only screen and (max-width: 768px) {
-		footer .fb-foot {
-			padding-top: 6px;}
-		}
+		footer{height: auto;}
+		footer .wrapper{flex-wrap: wrap;		}
+		.dep_market{ text-align: left; margin: 10px;}
+		footer .logo-smart {width: 70px;  height: 36px;background-size: cover;}
+	}
 	</style>
